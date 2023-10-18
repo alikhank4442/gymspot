18dbb60485683ba098bcb9345672827210c8d99f
alikhank4442 <148371155+alikhank4442@users.noreply.github.com>
2023-10-19 02:19:23 +0500

    Update credentials.py

--- gymspotify/credentials.py
+++ gymspotify/credentials.py
@@ -1,4 +1,5 @@
 CLIENT_ID = "e2b795abb5d346a98d116a51ed4295df"
 CLIENT_SECRET = "e6cb2998054f4aedbd8b3b31d499ab7c"
-REDIRECT_URI = "http://localhost:8000"
-REDIRECT_URI = "http://localhost:8000/redirect"
+#REDIRECT_URI = "http://localhost:8000"
+#REDIRECT_URI = "http://localhost:8000/redirect"
+REDIRECT_URI = https://gymspot.vercel.app/redirect
